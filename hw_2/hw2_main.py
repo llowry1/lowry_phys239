a49ab81043a62ef3a03116906d97a8036db40308
llowry915 <llowry@ucsd.edu>
2016-10-08 14:09:39 -0700

    Testing "Upload Files" option

--- hw_2/hw2_main.py
+++ hw_2/hw2_main.py
@@ -9,16 +9,20 @@ import numpy
 depth = 100 #[pc]
 n = 1 #[cm^-1]
 
+
+
 #Question1
 print "Question 1:"
 
+#find the column density
 colDens = func.findColDensity(n, depth)
 print "\tColumn Density of the cloud: ", colDens, " cm^-2"
 
+#find the cross sections for given optical depths
 optDepths = numpy.array([1e-2, 1, 1e2])
 crossSects = func.findCrossSection(colDens, optDepths)
 print "\tCross Section assuming total optical depth of..."
-print "\t\ta) 10^-3: ", crossSects[0]
-print "\t\tb) 1: ", crossSects[1]
-print "\t\tc) 10^3: ", crossSects[2]
+print "\t\ta) 10^-3: ", crossSects[0], " cm^2"
+print "\t\tb) 1: ", crossSects[1], " cm^2"
+print "\t\tc) 10^3: ", crossSects[2], " cm^2"
 
