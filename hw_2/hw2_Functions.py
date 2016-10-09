923fdf3eab5444826e61aaeec59f688b0a9d21a6
llowry915 <llowry@ucsd.edu>
2016-10-09 01:00:45 -0700

    Adding functionality for Question 2

--- hw_2/hw2_Functions.py
+++ hw_2/hw2_Functions.py
@@ -4,17 +4,70 @@ from astropy import constants
 
 
 #Determine the column density in cm^-2 assuming a uniform number density
-#params: number density [cm^-3], depth [pc]
-#return: column density [cm^-2]
-def findColDensity(n, depth):
+##params: n = number density [cm^-3], d = depth [pc]
+##return: column density [cm^-2]
+def findColDensity(n, d):
 	#convert pc to cm
-	depth = conv.pc_cm(depth)
+	d = conv.pc_cm(d)
 	#return column density
-	return depth*n
+	return d*n
 
 #Determine the cross section in cm^2 given column density and optical depth
-#params: column density [cm^-2], optical depth
-#return: cross section [cm^2]
+##params: colDens = column density [cm^-2], optDepth = optical depth
+##return: cross section [cm^2]
 def findCrossSection(colDens, optDepth):
 	#return cross section
 	return optDepth/colDens
+
+#Determine the absorption coefficient given number density and cross section
+##params: n = number density [cm^-3], crossSect = cross section [cm^2]
+##return: absorption coefficient [cm^-1]
+def findAbsorptionCoeff(n, crossSect):
+	return n*crossSect
+
+
+
+#Given the number density, cross section, initial intensity, and source funtion determine the specific intensity a distance d away for a single frequency
+#Uses given number of steps to incrementally calculate the specific intensity
+##params: numSteps = number of steps to calculate at, n = number density [cm^-1], crossSect = cross section [cm^2], initI = initial intensity [W/m^2/Hz/sr], sourceFunc = source function [W/m^2/Hz/sr], d = total distance d [pc]
+##return: the specific intensity at distance d [W/m^2/Hz/sr]
+def findSpecificIntensity_singleFreq(numSteps, n, crossSect, initI, sourceFunc, d):
+	
+	#Eq of Radiative Transfer:
+	#	dI_nu = alpha_nu*(S_nu - I_nu)*ds
+
+	#convert pc to cm
+	d = conv.pc_cm(d)
+	print "d in cm: ", d
+
+	#form arrays to hold distances and the intensity at each distance, and determine the distance step in cm
+	dists, ds = numpy.linspace(0, d, num = numSteps, retstep = True)
+	print "ds in cm: ", ds
+	intensities = numpy.zeros(len(dists))
+	intensities[0] = initI
+
+	#calculate the absorption coefficient
+	alpha_nu = findAbsorptionCoeff(n, crossSect)
+	print "alpha_nu in cm^-1: ", alpha_nu
+
+	#loop through the distance steps and calculate the change in specific intensity relative to the previous step
+	currI = initI
+	for i in range(len(intensities)):
+		intensities[i] = currI
+		print "currI: ", currI
+		print "intensities[",i,"]: ", intensities[i]
+
+		dI = alpha_nu*(sourceFunc - currI)*ds
+		nextI = currI + dI
+		print "nextI: ", nextI
+
+		currI = nextI
+	
+	#print the specific intensity at distance d
+	print intensities
+	print intensities[len(intensities) - 1]
+
+	return intensities[-1]
+
+
+	
--- hw_2/hw2_main.py
+++ hw_2/hw2_main.py
@@ -26,3 +26,20 @@ print "\t\ta) 10^-3: ", crossSects[0], " cm^2"
 print "\t\tb) 1: ", crossSects[1], " cm^2"
 print "\t\tc) 10^3: ", crossSects[2], " cm^2"
 
+
+#Question 2
+print "\nQuestion 2:"
+print "\tSee findSpecificIntensity_singleFreq function in hw2_Functions.py"
+
+#find final specific intensity assumptions listed below
+I_d = func.findSpecificIntensity_singleFreq(1000, n, 1e-20, 1.0, 0.5, depth)
+print "\tI(d = 100pc) = ", I_d
+print "\tAssumptions: 1000 steps, cross section = 10^-20 cm^2, I_0 = 1 W/m^2/Hz/sr, and S_nu = 0.5 W/m^2/Hz/sr"
+
+#Question 3
+print "\nQuestion 3:"
+
+
+#Question 4
+print "\nQuestion 4:"
+
