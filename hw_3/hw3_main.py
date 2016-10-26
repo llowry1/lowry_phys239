7fcdf3215506431ccda67699814f4b6f623916a7
Lindsay Ng Lowry <llowry@ucsd.edu>
2016-10-25 21:57:06 -0700

    Finished Question 5 and cleaned up script

--- hw_3/hw3_main.py
+++ hw_3/hw3_main.py
@@ -115,7 +115,7 @@ for time in times:
 
 ##Plot positions
 plt.figure(1)
-plt.title("Position")
+plt.title("Question 3 - Position")
 plt.xlabel("x position (a0)")
 plt.ylabel("y position (a0)")
 
@@ -135,44 +135,58 @@ plt.axvline(x=0, color = 'k')
 plt.plot(0,0, 'r8', markersize = 10)
 
 ##Plot velocities
+#plt.figure(2)
+#plt.title("Question 3 - Velocity")
+#plt.xlabel("x velocity (a0/s)")
+#plt.ylabel("y velocity (a0/s)")#
+#
+#i2 = 0
+#for num in velX:
+#	if i2 == 0:
+#		plt.plot(velX[i2], velY[i2], 'r*', markersize = 15)
+#	else:
+#		plt.plot(velX[i2], velY[i2], 'b.')
+#	i2 = i2 + 1
+#
+#plt.grid()
+#plt.axhline(y=0, color = 'k')
+#plt.axvline(x=0, color = 'k')
+
+##Plot velocities as a function of time
 plt.figure(2)
-plt.title("Velocity")
-plt.xlabel("x velocity (a0/s)")
-plt.ylabel("y velocity (a0/s)")#
-
-i2 = 0
-for num in velX:
-	if i2 == 0:
-		plt.plot(velX[i2], velY[i2], 'r*', markersize = 15)
-	else:
-		plt.plot(velX[i2], velY[i2], 'b.')
-	i2 = i2 + 1
-
-plt.grid()
-plt.axhline(y=0, color = 'k')
-plt.axvline(x=0, color = 'k')
+plt.suptitle("Question 3 - Velocity Components")
+plt.subplot(121)
+plt.title("Velocity x")
+plt.xlabel("time (s)")
+plt.ylabel("x velocity (a0/s^2)")
+plt.plot(times, velX)
+plt.subplot(122)
+plt.title("Velocity y")
+plt.xlabel("time (s)")
+plt.ylabel("y velocity (a0/s^2)")
+plt.plot(times, velY)
 
 ##Plot accelerations
-plt.figure(3)
-plt.title("Acceleration")
-plt.xlabel("x acceleration (a0/s^2)")
-plt.ylabel("y acceleration (a0/s^2)")
-
-i3 = 0
-for num in accX:
-	if i3 == 0:
-		plt.plot(accX[i3], accY[i3], 'r*', markersize = 15)
-	else:
-		plt.plot(accX[i3], accY[i3], 'b.')
-	i3 = i3 + 1
-
-plt.grid()
-plt.axhline(y=0, color = 'k')
-plt.axvline(x=0, color = 'k')
+#plt.figure(3)
+#plt.title("Question 3 - Acceleration")
+#plt.xlabel("x acceleration (a0/s^2)")
+#plt.ylabel("y acceleration (a0/s^2)")
+#
+#i3 = 0
+#for num in accX:
+#	if i3 == 0:
+#		plt.plot(accX[i3], accY[i3], 'r*', markersize = 15)
+#	else:
+#		plt.plot(accX[i3], accY[i3], 'b.')
+#	i3 = i3 + 1
+#
+#plt.grid()
+#plt.axhline(y=0, color = 'k')
+#plt.axvline(x=0, color = 'k')
 
 ##Plot accelerations as a function of time
 plt.figure(4)
-plt.suptitle("Acceleration Components")
+plt.suptitle("Question 3 - Acceleration Components")
 plt.subplot(121)
 plt.title("Acceleration x")
 plt.xlabel("time (s)")
@@ -199,7 +213,7 @@ freqs = numpy.fft.fftfreq(n, timestep)
 #print "Freq Step: ", freqs[-1] - freqs[-2]
 
 plt.figure(5)
-plt.suptitle("FT: Acceleration X")
+plt.suptitle("Question 4 - FT: Acceleration X")
 plt.subplot(121)
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Power (arbitrary units)")
@@ -215,7 +229,7 @@ plt.xlim(0, 2e12)
 
 ftY = numpy.fft.fft(accY)
 plt.figure(6)
-plt.suptitle("FT: Acceleration Y")
+plt.suptitle("Question 4 - FT: Acceleration Y")
 plt.subplot(121)
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Power (arbitrary units)")
@@ -234,7 +248,7 @@ acc = numpy.sqrt(numpy.square(accX) + numpy.square(accY))
 ft = numpy.fft.fft(acc)
 
 plt.figure(7)
-plt.suptitle("FT: Acceleration (a = ((a_x)^2 + (a_y)^2)^(1/2))")
+plt.suptitle("Question 4 - FT: Acceleration (a = ((a_x)^2 + (a_y)^2)^(1/2))")
 plt.subplot(121)
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Power (arbitrary units)")
@@ -251,17 +265,20 @@ plt.show()
 
 ##Question 5--------------------------------------------------------------------------------------------------------
 print "\nQuestion 5:"
-print "See plots of peak frequency vs. b and v0"
+print "\tSee plots of peak frequency vs. b and v0"
+print "\t*Note: the limited resolution of the Fourier transform constrains the resolution with which we can determine the peak frequency, but the general trend is still visible."
 
-
+x0 = -250000
+y0 = 5000
+Z = 4
 ##Determine how changing b affects the peak frequency
 
 ##Specify range of impact parameters to look at
-impactParams = numpy.arange(2000, 10000, 100)
+impactParams = numpy.linspace(2000, 10000, 100)
 vel0 = numpy.array([conv.cm_bohr(5e7), 0.0])
 
 ##Make an array to hold the peak frequencies associated with each impact parameter
-peakFreqs = numpy.zeros(len(impactParams))
+bPeakFreqs = numpy.zeros(len(impactParams))
 
 i4 = 0
 for b in impactParams:
@@ -315,16 +332,123 @@ for b in impactParams:
     n = accX.size
     freqs = numpy.fft.fftfreq(n, timestep)
     
+    ftXb = numpy.square(numpy.abs(ftXb))
+    
+    freqs = freqs[0:n/2]
+    ftXb = ftXb[0:n/2]
+    
     peak = numpy.amax(ftXb)
     w = numpy.where(ftXb == peak)
     w1 = w[0][0]
-    print w1
-    peakFreqs[i4] = freqs[w1]
-    print peakFreqs[i4]
+
+    bPeakFreqs[i4] = freqs[w1]
+
+    
+    #plt.figure()
+    #plt.plot(freqs, ftXb)
+    #plt.plot(freqs, ftXb, 'r.')
+    #plt.plot(freqs[w1], peak, 'go')
+    #plt.show()
     
     i4 = i4+1
     
-#plt.figure()
-#plt.plot(b, peakFreqs)
-#
-#plt.show()
+
+##Determine how changing v0 affects the peak frequency
+
+##Specify range of impact parameters to look at
+velocities = numpy.linspace(3e7, 1e8, 100)
+pos0 = numpy.array([x0,y0])
+
+##Make an array to hold the peak frequencies associated with each initial velocity
+vPeakFreqs = numpy.zeros(len(velocities))
+
+i5 = 0
+for v in velocities:
+    vel0 = numpy.array([conv.cm_bohr(v), 0.0])
+    
+    ##make arrays to hold the times and corresponding position, velocity, and acceleration components
+    num = 10000
+    times, timestep = numpy.linspace(0, 5e-11, num, retstep = True) #[s]
+    #posX = numpy.zeros(len(times))
+    #posY = numpy.zeros(len(times))
+    #velX = numpy.zeros(len(times))
+    #velY = numpy.zeros(len(times))
+    accX = numpy.zeros(len(times))
+    accY = numpy.zeros(len(times))
+    
+    ##set initial position, velocity, and acceleration
+    #posX[0] = pos0[0]
+    #posY[0] = pos0[1]
+    #velX[0] = vel0[0]
+    #velY[0] = vel0[1]
+    
+    acc0 = func.getAccel(func.coulombForce(1, Z, pos0))
+    accX[0] = acc0[0]
+    accY[0] = acc0[1]
+    
+    
+    i = 0
+    for time in times:
+   	if i == 0:
+  		prevPos = pos0
+  		prevVel = vel0
+  		prevAcc = acc0	
+   	else:
+  		vel = prevVel + prevAcc*timestep
+  		#velX[i] = vel[0]
+  		#velY[i] = vel[1]
+  		pos = prevPos + vel*timestep
+  		#posX[i] = pos[0]
+  		#posY[i] = pos[1]
+  		acc = func.getAccel(func.coulombForce(1, Z, pos))
+  		accX[i] = acc[0]
+  		accY[i] = acc[1]
+  		
+  		prevPos = pos
+  		prevVel = vel
+  		prevAcc = acc
+  		
+   	i = i+1
+   
+    ##take the FT of the x component of the acceleration
+    ftXb = numpy.fft.fft(accX)
+    n = accX.size
+    freqs = numpy.fft.fftfreq(n, timestep)
+    
+    ftXb = numpy.square(numpy.abs(ftXb))
+    
+    freqs = freqs[0:n/2]
+    ftXb = ftXb[0:n/2]
+    
+    peak = numpy.amax(ftXb)
+    w = numpy.where(ftXb == peak)
+    w1 = w[0][0]
+
+    vPeakFreqs[i5] = freqs[w1]
+
+    
+    #plt.figure()
+    #plt.plot(freqs, ftXb)
+    #plt.plot(freqs, ftXb, 'r.')
+    #plt.plot(freqs[w1], peak, 'go')
+    #plt.show()
+    
+    i5 = i5+1
+    
+plt.figure(8)
+plt.suptitle("Question 5")
+plt.subplot(121)
+plt.title("Dependence on Impact Paramter b")
+plt.xlabel("Impact parameter b (a0)")
+plt.ylabel("Peak Frequency (Hz)")
+plt.ylim(0.5e11, 5.0e11)
+plt.plot(impactParams, bPeakFreqs, 'go')
+
+plt.subplot(122)
+plt.title("Dependence on Initial Velocity v0")
+plt.xlabel("Initial Velocity v0_x (a0/s)")
+plt.ylabel("Peak Frequency (Hz)")
+plt.ylim(0.5e11, 5.0e11)
+plt.plot(velocities, vPeakFreqs, 'mo')
+
+plt.show()
--- hw_3/hw3_question5.py
+++ hw_3/hw3_question5.py
@@ -19,7 +19,7 @@ impactParams = numpy.linspace(2000, 10000, 100)
 vel0 = numpy.array([conv.cm_bohr(5e7), 0.0])
 
 ##Make an array to hold the peak frequencies associated with each impact parameter
-peakFreqs = numpy.zeros(len(impactParams))
+bPeakFreqs = numpy.zeros(len(impactParams))
 
 i4 = 0
 for b in impactParams:
@@ -82,7 +82,7 @@ for b in impactParams:
     w = numpy.where(ftXb == peak)
     w1 = w[0][0]
 
-    peakFreqs[i4] = freqs[w1]
+    bPeakFreqs[i4] = freqs[w1]
 
     
     #plt.figure()
@@ -93,9 +93,101 @@ for b in impactParams:
     
     i4 = i4+1
     
-plt.figure()
+    
+    
+##Determine how changing v0 affects the peak frequency
+y0 = 5000
+##Specify range of impact parameters to look at
+velocities = numpy.linspace(3e7, 1e8, 100)
+pos0 = numpy.array([x0,y0])
+
+##Make an array to hold the peak frequencies associated with each initial velocity
+vPeakFreqs = numpy.zeros(len(velocities))
+
+i5 = 0
+for v in velocities:
+    vel0 = numpy.array([conv.cm_bohr(v), 0.0])
+    
+    ##make arrays to hold the times and corresponding position, velocity, and acceleration components
+    num = 10000
+    times, timestep = numpy.linspace(0, 5e-11, num, retstep = True) #[s]
+    #posX = numpy.zeros(len(times))
+    #posY = numpy.zeros(len(times))
+    #velX = numpy.zeros(len(times))
+    #velY = numpy.zeros(len(times))
+    accX = numpy.zeros(len(times))
+    accY = numpy.zeros(len(times))
+    
+    ##set initial position, velocity, and acceleration
+    #posX[0] = pos0[0]
+    #posY[0] = pos0[1]
+    #velX[0] = vel0[0]
+    #velY[0] = vel0[1]
+    
+    acc0 = func.getAccel(func.coulombForce(1, Z, pos0))
+    accX[0] = acc0[0]
+    accY[0] = acc0[1]
+    
+    
+    i = 0
+    for time in times:
+   	if i == 0:
+  		prevPos = pos0
+  		prevVel = vel0
+  		prevAcc = acc0	
+   	else:
+  		vel = prevVel + prevAcc*timestep
+  		#velX[i] = vel[0]
+  		#velY[i] = vel[1]
+  		pos = prevPos + vel*timestep
+  		#posX[i] = pos[0]
+  		#posY[i] = pos[1]
+  		acc = func.getAccel(func.coulombForce(1, Z, pos))
+  		accX[i] = acc[0]
+  		accY[i] = acc[1]
+  		
+  		prevPos = pos
+  		prevVel = vel
+  		prevAcc = acc
+  		
+   	i = i+1
+   
+    ##take the FT of the x component of the acceleration
+    ftXb = numpy.fft.fft(accX)
+    n = accX.size
+    freqs = numpy.fft.fftfreq(n, timestep)
+    
+    ftXb = numpy.square(numpy.abs(ftXb))
+    
+    freqs = freqs[0:n/2]
+    ftXb = ftXb[0:n/2]
+    
+    peak = numpy.amax(ftXb)
+    w = numpy.where(ftXb == peak)
+    w1 = w[0][0]
+
+    vPeakFreqs[i5] = freqs[w1]
+
+    
+    #plt.figure()
+    #plt.plot(freqs, ftXb)
+    #plt.plot(freqs, ftXb, 'r.')
+    #plt.plot(freqs[w1], peak, 'go')
+    #plt.show()
+    
+    i5 = i5+1
+    
+plt.figure(8)
+plt.subplot(121)
 plt.title("Dependence on Impact Paramter b")
 plt.xlabel("Impact parameter b (a0)")
 plt.ylabel("Peak Frequency (Hz)")
-plt.plot(impactParams, peakFreqs, 'go')
+plt.plot(impactParams, bPeakFreqs, 'go')
+
+plt.subplot(122)
+plt.title("Dependence on Initial Velocity v0")
+plt.xlabel("Initial Velocity v0_x (a0/s)")
+plt.ylabel("Peak Frequency (Hz)")
+plt.plot(velocities, vPeakFreqs, 'mo')
+
 plt.show()
