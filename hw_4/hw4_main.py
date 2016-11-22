058c09f75ed64340ea8221ae8ab684c1d1636898
Lindsay Ng Lowry <llowry@ucsd.edu>
2016-11-22 11:26:14 -0800

    Adding test files

--- hw_4/hw4_main.py
+++ hw_4/hw4_main.py
@@ -1,3 +1,21 @@
 import numpy
 import matplotlib.pyplot as plt
 import hw4_Functions as func
+import os
+
+##Script to output answers to questions on HW4
+
+
+
+##Question 1--------------------------------------------------------------------------------------------------------
+print "Question 1:"
+
+#datafile = "m82spec_formatted.txt"
+datafile = "test.txt"
+
+
+##Load the downloaded data into arrays
+data = numpy.loadtxt(datafile,delimiter=',')
+
+
+print data
